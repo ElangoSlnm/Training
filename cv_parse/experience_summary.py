25c7baaa04d64fef4bbe3411009c0ee9cf482e18
ElangoSlnm <42702028+ElangoSlnm@users.noreply.github.com>
2020-01-31 21:39:47 +0530

    extract experience summary

--- cv_parse/experience_summary.py
+++ cv_parse/experience_summary.py
@@ -1,37 +1,33 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
 import spacy
 from spacy.lang.en import English
 nlp = English()
-nlp.add_pipe(nlp.create_pipe('sentencizer'))
 import re
+nlp.add_pipe(nlp.create_pipe('sentencizer'))
+nlp_lg = spacy.load("en_core_web_lg")
 
-
-# %%
-with open(r'D:\NLP\cv_parse\content.txt', 'r') as reader:
+with open(r'D:\NLP\cv_parse\ashutosh.txt', 'r') as reader:
     doc_text = reader.read()
 
-
-# %%
-nlp_mdl = spacy.load("en_core_web_lg")
-
-
-# %%
-regex = re.compile(r'[\n\r\t\x0c]')
+regex = re.compile(r'[\r\t\x0c]')
 raw_text = regex.sub(' ', doc_text)
 doc = nlp(raw_text)
-sentences = [sent.string.strip() for sent in doc.sents if re.search('Experience|[\d]{1,2}/[\d]{1,2}/[\d]{4}|[\d]{1,2}-[\d]{1,2}-[\d]{2}|[ADFJMNOS]\w* [\d]{4}', str(sent), re.I)]
+sentences = [sent.string.strip() for sent in doc.sents if re.search('Experience|[ADFJMNOS]\w* [\d]{4}|[ADFJMNOS]\w*-[\d]{4}', str(sent), re.I)]
 
+experience_list = []
+for sent_ in sentences:
+    for sent in sent_.split('.'):
+        experience = []
+        date = []
+        org = []
+        for ents in nlp_lg(sent.replace('\n',' ').replace('-', ' ').replace('   ',' ').replace('  ', ' ').replace('\t', ' ')).ents:
+            if ents.label_ == 'DATE' and re.search('[ADFJMNOS]\w* [\d]{4}|year|mont|date', str(ents.text), re.I):
+                date.append(ents.text)
+            elif ents.label_ == 'ORG':
+                org.append(ents.text)
+        if len(date)>0 and len(org) >0:
+            experience.append(' - '.join(date))
+            experience.append(', '.join(org))
+        if len(experience)>0:
+            experience_list.append(experience)
 
-# %%
-print(sentences)
-
-
-# %%
-experience = []
-for sent in sentences:
-    for ents in nlp_mdl(sent).ents:
-        print(ents.text, ents.label_)
-    # doc = nlp(sentences)
-
+print(experience_list)
